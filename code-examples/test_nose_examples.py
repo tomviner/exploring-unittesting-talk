9b36e2aa3dba83dfab4da40ac6fa59d7590d44c0
TomV <tom@viner.tv>
2014-09-15 09:22:43 +0100

    more work on talk

--- code-examples/test_nose_examples.py
+++ code-examples/test_nose_examples.py
@@ -10,3 +10,11 @@ def test_add():
 
 def test_validation():
     nose.tools.assert_raises(TypeError, add, ('a', 1))
+
+def test_validation_with():
+    with nose.tools.assert_raises(TypeError) as e:
+        add('a', 1)
+
+@nose.tools.raises(TypeError)
+def test_validation_decorator():
+    add('a', 1)
